b72d2565bbeab9278b754c3af78e35bdeb066a48
Marco Marchi <115740827+MarquinCss@users.noreply.github.com>
2023-12-23 01:41:03 -0300

    Add files via upload

---
+++ js/vendas.js
@@ -0,0 +1,32 @@
+var codigoCorreto = "3241";
+
+function exibirModal() {
+    var modal = document.getElementById("modal");
+    var mainContent = document.querySelector(".main-content");
+
+    modal.style.display = "block";
+    mainContent.classList.add("blurred"); // Adiciona a classe para desfocar o conteúdo
+}
+
+function fecharModal() {
+    var modal = document.getElementById("modal");
+    var mainContent = document.querySelector(".main-content");
+
+    modal.style.display = "none";
+    mainContent.classList.remove("blurred"); // Remove a classe de desfoque
+}
+
+function verificarCodigo() {
+    var codigoInput = document.getElementById("codigoInput");
+    var codigoDigitado = codigoInput.value;
+
+    if (codigoDigitado === codigoCorreto) {
+        fecharModal();
+    } else {
+        alert("Código incorreto. Tente novamente.");
+        codigoInput.value = ""; // Limpar o campo de entrada
+    }
+}
+
+// Exibir o modal ao carregar a página (você pode chamar essa função quando necessário)
+window.onload = exibirModal;
