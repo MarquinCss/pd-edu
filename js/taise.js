d154792be954a0a0e0f92ccfff36559fe5f08c70
Marco Marchi <antoniomarchi2006@gmail.com>
2023-12-24 22:16:31 -0300

    :new:  Atualização

--- js/taise.js
+++ js/taise.js
@@ -1,31 +1,31 @@
 var codigoCorreto = "1111";
+var paragrafoErro = document.querySelector("#erro");
+var modal = document.getElementById("modal");
 
 function exibirModal() {
-    var modal = document.getElementById("modal");
-    var mainContent = document.querySelector(".main-content");
-
-    modal.style.display = "block";
-    mainContent.classList.add("blurred"); // Adiciona a classe para desfocar o conteúdo
+  modal.style.display = "block";
 }
 
 function fecharModal() {
-    var modal = document.getElementById("modal");
-    var mainContent = document.querySelector(".main-content");
-
-    modal.style.display = "none";
-    mainContent.classList.remove("blurred"); // Remove a classe de desfoque
+  modal.style.display = "none";
 }
 
 function verificarCodigo() {
-    var codigoInput = document.getElementById("codigoInput");
-    var codigoDigitado = codigoInput.value;
+  var codigoInput = document.getElementById("codigoInput");
+  var codigoDigitado = codigoInput.value;
 
-    if (codigoDigitado === codigoCorreto) {
-        fecharModal();
-    } else {
-        alert("Código incorreto. Tente novamente.");
-        codigoInput.value = ""; // Limpar o campo de entrada
-    }
+  if (codigoDigitado === codigoCorreto) {
+    paragrafoErro.innerHTML = "";
+    fecharModal();
+  } else {
+    paragrafoErro.innerHTML = "Erro, tente novamente, jovem!";
+    codigoInput.value = "";
+    paragrafoErro.classList.add("shake"); // Adiciona a classe de animação ao modal
+    modal.classList.add("shake"); // Adiciona a classe de animação ao modal
+    setTimeout(function () {
+      modal.classList.remove("shake"); // Remove a classe após a animação
+    }, 500); // Tempo igual à duração da animação
+  }
 }
 
 // Exibir o modal ao carregar a página (você pode chamar essa função quando necessário)
